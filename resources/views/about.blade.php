3192127fa3cfdbf96e5c9324b82406e6fa08ee02
shababhsiddique <shabab.h.siddique@gmail.com>
2018-02-01 20:47:12 +0600

    Setup Laravel Blog Project on Git

---
+++ resources/views/about.blade.php
@@ -0,0 +1,8 @@
+<html>
+    <head>
+        
+    </head>
+    <body>
+        <h3>About Page</h3>
+    </body>
+</html>
---
+++ resources/views/master.blade.php
@@ -0,0 +1,8 @@
+<html>
+    <head>
+        
+    </head>
+    <body>
+        <h3>Master Page</h3>
+    </body>
+</html>
